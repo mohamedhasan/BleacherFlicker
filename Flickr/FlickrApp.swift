c6c841b2fcf641e5c7a79a149ac8dcf09cecd11f
Mohamed Hassan <mohamedha@backbase.com>
2023-02-02 14:55:56 +0200

    Initial Commit

---
+++ Flickr/FlickrApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrApp.swift
+//  Flickr
+//
+//  Created by Mohamed Hassan on 02/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlickrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
